5bb649f1dfc719e15e5de2ff71b5b36cca7d8675
Chibbis <46288058+thechibbis@users.noreply.github.com>
2022-01-16 02:12:33 -0300

    added genero

--- testeJS/js/script.js
+++ testeJS/js/script.js
@@ -1,7 +1,10 @@
+//just a simple test with JS
+
 let nome = prompt("digite seu nome");
 let sobrenome = prompt("digite seu sobrenome");
-let idade = prompt("digite sua idade");
+let idade = prompt("digite sua idade");]
+let genero = prompt("digite seu genero");
 
-console.log(`Ei bixa ou devo dizer ${nome} ${sobrenome} ta fazendo oq aq ???`);
+console.log(`Ei cara ou devo dizer ${nome} ${sobrenome} fiquei sabendo que você é ${genero} ta fazendo oq aq ???`);
 
 alert(`Olá ${nome} ${sobrenome} e vocẽ tem a idade equivalente á: ${idade} anos!`);
