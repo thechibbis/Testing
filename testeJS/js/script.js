cb1b2a65232c016608bb61ff7ec37b0abdf6d47f
Chibbis <46288058+thechibbis@users.noreply.github.com>
2022-01-16 02:07:50 -0300

    Add files via upload

---
+++ testeJS/js/script.js
@@ -0,0 +1,7 @@
+let nome = prompt("digite seu nome");
+let sobrenome = prompt("digite seu sobrenome");
+let idade = prompt("digite sua idade");
+
+console.log(`Ei bixa ou devo dizer ${nome} ${sobrenome} ta fazendo oq aq ???`);
+
+alert(`Olá ${nome} ${sobrenome} e vocẽ tem a idade equivalente á: ${idade} anos!`);
